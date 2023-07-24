eba191188dd0c4a1f0941dc655672409f81d1029
teawon <xodnjsvkvk9@gmail.com>
2023-07-24 22:53:18 +0900

    2장 컴포넌트 사전로딩 (useEffect)

--- lecture-2/src/App.js
+++ lecture-2/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, Suspense, lazy } from "react";
+import React, { useState, Suspense, lazy, useEffect } from "react";
 import styled from "styled-components";
 import Header from "./components/Header";
 import InfoTable from "./components/InfoTable";
@@ -10,18 +10,23 @@ const LazyImageModal = lazy(() => import("./components/ImageModal"));
 function App() {
   const [showModal, setShowModal] = useState(false);
 
-  const handleMouseEnter = () => {
+  useEffect(() => {
     import("./components/ImageModal").catch();
-    // 네트워크 요청이 이미 완료되었다면, 같은 모듈을 다시 요청할 때 네트워크 요청을 다시 수행하지 않는다.
-    // 따라서 여기서 이미 모듈을 가져오면 ,브라우저나 JavaScript 엔진은 이전에 불러온 모듈의 캐시된 본을 사용한다
-  };
+    // Hover보다 더 빠르게, 모든 컴포넌트가 마운트 완료되면 추가로 로드
+  }, []);
+
+  // const handleMouseEnter = () => {
+  //   import("./components/ImageModal").catch();
+  //   // 네트워크 요청이 이미 완료되었다면, 같은 모듈을 다시 요청할 때 네트워크 요청을 다시 수행하지 않는다.
+  //   // 따라서 여기서 이미 모듈을 가져오면 ,브라우저나 JavaScript 엔진은 이전에 불러온 모듈의 캐시된 본을 사용한다
+  // };
 
   return (
     <div className="App">
       <Header />
       <InfoTable />
       <ButtonModal
-        onMouseEnter={handleMouseEnter} //hover시 미리 ImageModal 컴포넌트를 사전에 로딩한다.
+        // onMouseEnter={handleMouseEnter} //hover시 미리 ImageModal 컴포넌트를 사전에 로딩한다.
         onClick={() => {
           setShowModal(true);
         }}
