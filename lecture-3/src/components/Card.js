efde8d885ab13fea39d5609296cc02024ec7feb0
teawon <xodnjsvkvk9@gmail.com>
2023-08-12 01:02:59 +0900

    3장 - 이미지  지연로딩

--- lecture-3/src/components/Card.js
+++ lecture-3/src/components/Card.js
@@ -1,14 +1,36 @@
-import React from 'react'
+import React, { useEffect, useRef } from "react";
 
 function Card(props) {
-	return (
-		<div className="Card text-center">
-			<img src={props.image}/>
-			<div className="p-5 font-semibold text-gray-700 text-xl md:text-lg lg:text-xl keep-all">
-				{props.children}
-			</div>
-		</div>
-	)
+  const imgRef = useRef(null);
+
+  useEffect(() => {
+    // 하나의 인스턴스만 생성하도록 useEffect를 사용
+    const options = {};
+    const callback = (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          console.log("is intersecting", entry.target.dataset.src);
+          entry.target.src = entry.target.dataset.src;
+          observer.unobserve(entry.target);
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(callback, options);
+
+    observer.observe(imgRef.current);
+
+    return () => observer.disconnect(); // clean up 함수를 사용해 리소스 낭비를 줄인다
+  }, []);
+
+  return (
+    <div className="Card text-center">
+      <img data-src={props.image} ref={imgRef} />
+      <div className="p-5 font-semibold text-gray-700 text-xl md:text-lg lg:text-xl keep-all">
+        {props.children}
+      </div>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
