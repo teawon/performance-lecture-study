d43ae3e6f415dc2710bb7c3a65d72d7162e34b86
teawon <xodnjsvkvk9@gmail.com>
2023-08-12 19:59:27 +0900

    3장 이미지 지연로딩 추가 공부 및 주석

--- lecture-3/src/components/Card.js
+++ lecture-3/src/components/Card.js
@@ -5,7 +5,11 @@ function Card(props) {
 
   useEffect(() => {
     // 하나의 인스턴스만 생성하도록 useEffect를 사용
-    const options = {};
+    const options = {
+      // root: null,                                 // 뷰포트 요소 설정( null 이면 기본 브라우저)
+      // threshhold: 0,                              // 0~1 사이의 값으로, 이미지 노출 비율을 의미한다. 1이면 전부 보여야 실행
+      // rootMargin: "1000px 1000px 1000px 1000px",  // 이미지가 미리 로딩되도록 여유분을 준다
+    };
     const callback = (entries, observer) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
@@ -25,7 +29,12 @@ function Card(props) {
 
   return (
     <div className="Card text-center">
-      <img data-src={props.image} ref={imgRef} />
+      {/* lazy옵션을 통해 직접 넣으면, 브라우저내에서 자체적으로 해당 이미지를 지연로딩시킨다. 단 어느정도 여백을 남기고 미리 받는듯 */}
+      <img
+        data-src={props.image}
+        ref={imgRef}
+        //    loading="lazy"
+      />
       <div className="p-5 font-semibold text-gray-700 text-xl md:text-lg lg:text-xl keep-all">
         {props.children}
       </div>
