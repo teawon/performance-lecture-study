1314788065a830247506e55e9df6ea3176fdd351
teawon <xodnjsvkvk9@gmail.com>
2023-08-19 22:53:34 +0900

    3장 - 폰트 최적화 : 시점 조절

--- lecture-3/src/components/BannerVideo.js
+++ lecture-3/src/components/BannerVideo.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import video from "../assets/banner-video.mp4";
 import video_webm from "../assets/_banner-video.webm";
+import FontFaceObserver from "fontfaceobserver";
+
+const font = new FontFaceObserver("BMYEONSUNG");
 
 function BannerVideo() {
+  const [isFontLoaded, setIsFontLoaded] = useState(false);
+
+  useEffect(() => {
+    // 20초 타임아웃시 Promise에서 에러 발생
+    font.load(null, 20000).then(() => {
+      console.log("폰트 로드");
+      setIsFontLoaded(true);
+    });
+  }, []);
+
   return (
     <div className="BannerVideo w-full h-screen overflow-hidden relative bg-texture">
       <div className="absolute h-screen w-full left-1/2">
@@ -17,7 +30,13 @@ function BannerVideo() {
           <source src={video} type="video/mp4" />
         </video>
       </div>
-      <div className="w-full h-full flex justify-center items-center">
+      <div
+        className="w-full h-full flex justify-center items-center"
+        style={{
+          opacity: isFontLoaded ? 1 : 0,
+          transition: "opacity 0.3s ease",
+        }}
+      >
         <div className="text-white text-center">
           <div className="text-6xl leading-none font-semibold">KEEP</div>
           <div className="text-6xl leading-none font-semibold">CALM</div>
