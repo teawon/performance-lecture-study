a9d30d87617c6b54a3fbba16cdf592bf67c1a432
teawon <xodnjsvkvk9@gmail.com>
2023-08-21 22:37:49 +0900

    3장 캐시 최적화 : 특성에 따른 적절한 캐시 옵션과 시간 설정

--- lecture-3/server/server.js
+++ lecture-3/server/server.js
@@ -5,7 +5,20 @@ const path = require("path");
 
 const header = {
   setHeaders: (res, path) => {
-    res.setHeader("Cache-Control", 'max-age="10'); // 헤더에 캐시 정보를 사용한다. 10초동안 캐시를 적용하겠다는 의미
+    if (path.endsWith(".html")) {
+      res.setHeader("Cache-Control", "no-cache");
+      // no-cache를 사용하면 브라우저는 항상 원래 서버에 해당 리소스의 유효성을 확인하고, 변경되었을 경우 최진 버전을 제공해주어야한다
+    } else if (
+      path.endsWith(".js") ||
+      path.endsWith(".css") ||
+      path.endsWith(".webp")
+    ) {
+      res.setHeader("Cache-Control", "public, max-age=31536000");
+      // 정적 자원들은 자주 변경되지 않을 가능성이 높기 때문에 긴 시간을 설정한다
+    } else {
+      res.setHeader("Cache-Control", "no-store");
+      // no-store는 캐시에 어떠한 것도 저장되지 않도록 하는데 이는 매번 해당 자원을 서버에서 새로 가져와야 함을 의미한다.
+    }
   },
 };
 
