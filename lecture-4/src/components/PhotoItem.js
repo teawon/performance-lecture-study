d44271f876a98222fca3acf1ffcfcb777416eb0e
teawon <xodnjsvkvk9@gmail.com>
2023-08-29 01:10:00 +0900

    4장 이미지 갤러리 최적화 - 이미지 사이즈를 통한 레이아웃 이동 피하기

--- lecture-4/src/components/PhotoItem.js
+++ lecture-4/src/components/PhotoItem.js
@@ -17,11 +17,30 @@ function PhotoItem({ photo: { urls, alt } }) {
   );
 }
 
-const ImageWrap = styled.div``;
+const ImageWrap = styled.div`
+  width: 100%;
+  padding-bottom: 56.25%;
+  position: relative;
+`;
 
 const Image = styled.img`
+  position: absolute;
   cursor: pointer;
   width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
 `;
+
+// 또는 아래의 방법을 사용하기. 단 asept-ration은 호환성문제가 있다.
+// .wrapper {
+//   width: 100%;
+//   asept-ratio: 16 /9;
+// }
+
+// .image {
+//   width: 100%;
+//   height: 100%;
+// }
 
 export default PhotoItem;
