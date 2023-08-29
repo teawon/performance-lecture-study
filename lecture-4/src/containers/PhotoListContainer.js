6a660370db0ca5b152853b4519ca196a26f67c79
teawon <xodnjsvkvk9@gmail.com>
2023-08-29 22:52:37 +0900

    4장 이미지 최적화 - Redux 배열 참조에 의한 필터 결과 값 분리

--- lecture-4/src/containers/PhotoListContainer.js
+++ lecture-4/src/containers/PhotoListContainer.js
@@ -10,16 +10,17 @@ function PhotoListContainer() {
     dispatch(fetchPhotos());
   }, [dispatch]);
 
-  const { photos, loading } = useSelector(
+  const { category, allPhotos, loading } = useSelector(
     state => ({
-      photos:
-        state.category.category === 'all'
-          ? state.photos.data
-          : state.photos.data.filter(photo => photo.category === state.category.category),
+      category: state.category.category,
+      allPhotos: state.photos.data,
       loading: state.photos.loading,
     }),
     shallowEqual
   );
+
+  const photos =
+    category === 'all' ? allPhotos : allPhotos.filter(photo => photo.category === category); //필터 과정을 컴포넌트로 분리해야한다. 매번 필더링된 결과 배열로 인해 불필요한 렌더링이 발생하기때문
 
   if (loading === 'error') {
     return <span>Error!</span>;
