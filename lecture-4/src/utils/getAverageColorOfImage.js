bffb0dc42922c1a86c35cd79b28cb01b234d6b1e
teawon <xodnjsvkvk9@gmail.com>
2023-08-29 23:37:27 +0900

    4장 이미지 최적화 - 메모리제이션

--- lecture-4/src/utils/getAverageColorOfImage.js
+++ lecture-4/src/utils/getAverageColorOfImage.js
@@ -1,4 +1,10 @@
+const cache = {};
+
 export function getAverageColorOfImage(imgElement) {
+  if (cache.hasOwnProperty(imgElement.src)) {
+    return cache[imgElement.src];
+  }
+
   const canvas = document.createElement('canvas');
   const context = canvas.getContext && canvas.getContext('2d');
   const averageColor = {
@@ -28,9 +34,12 @@ export function getAverageColorOfImage(imgElement) {
   }
 
   const count = length / 4;
-  averageColor.r = ~~(averageColor.r / count); // ~~ => convert to int
+  averageColor.r = ~~(averageColor.r / count);
   averageColor.g = ~~(averageColor.g / count);
   averageColor.b = ~~(averageColor.b / count);
 
+  cache[imgElement.src] = averageColor;
+  // 이미 특정 src에 대한 연산을 수행했다면, 그 결과를 저장했다가 바로 return한다. (메모리제이션)
+  // 물론 처음에 느리다는건 변함이 없다. 새로고침해도 날라가고
   return averageColor;
 }
